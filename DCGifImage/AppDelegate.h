b1da38508d3608217ea7af4fa050c93a61fdf6e9
Dincoln <xuedingjun@ibona.cn>
2017-04-20 17:27:44 +0800

    Initial Commit

---
+++ DCGifImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DCGifImage
+//
+//  Created by Dincoln on 2017/4/20.
+//  Copyright © 2017年 Dincoln. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DCGifImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DCGifImage
+//
+//  Created by Dincoln on 2017/4/20.
+//  Copyright © 2017年 Dincoln. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
